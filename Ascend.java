9bbce3c638aed14da4b3fbb50253bb7d0af34b80
Omer Argaman <omer@Omers-MacBook-Air.local>
2024-11-06 18:12:10 +0200

    First submition of HW1

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,15 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int)(Math.random() * (lim + 1));
+		int b = (int)(Math.random() * (lim + 1));
+		int c = (int)(Math.random() * (lim + 1));	
+	    int min = Math.min(a, Math.min(b, c));
+		int max = Math.max(a, Math.max(b, c));
+		int mid = Math.min(Math.min(Math.max(a,b), Math.max(b,c)), Math.max(a,c));
+		System.out.println(a + " " + b + " " +c);
+		System.out.println(min + " " + mid + " " + max);
+		// Prints 3 random numbers and then prints them in increasing order.
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+        int hundreds = num / 100;
+        int tens = (num % 100) / 10;
+        int units = num % 10;
+        System.out.println(hundreds + " hundreds " + tens + 
+						" tens " + units + " units");
+		// Prints "1 hundreds 2 tens 3 units" if num = 123
 	}
 }
